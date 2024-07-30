8d5bbb816dfbe49859091a0af318f0c237d0b46d
hybridchamber <carloalabon80@gmail.com>
2024-07-30 12:38:15 +0800

    retrict the non-adin

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -21,7 +21,7 @@
               </SidebarLink>
             </li>
             <li>
-              <SidebarLink href="/users" :active="isActive('/users')" :class="{'bg-blue-500':isActive('/users'),'text-white':true}">
+              <SidebarLink href= "/users" :active="isActive('/users') " :class="{'bg-blue-500':isActive('/users'),'text-white':true}" v-if="hasRole('admin')">
                 <template #icon>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
@@ -31,7 +31,7 @@
               </SidebarLink>
             </li>
             <li>
-              <SidebarLink href="/roles" :active="isActive('/roles')" :class="{'bg-blue-500':isActive('/roles'),'text-white':true}">
+              <SidebarLink href="/roles" :active="isActive('/roles')" :class="{'bg-blue-500':isActive('/roles'),'text-white':true}" v-if="hasRole('admin')">
                 <template #icon>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12a7.5 7.5 0 0 0 15 0m-15 0a7.5 7.5 0 1 1 15 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077 1.41-.513m14.095-5.13 1.41-.513M5.106 17.785l1.15-.964m11.49-9.642 1.149-.964M7.501 19.795l.75-1.3m7.5-12.99.75-1.3m-6.063 16.658.26-1.477m2.605-14.772.26-1.477m0 17.726-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205 12 12m6.894 5.785-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495" />
@@ -41,7 +41,7 @@
               </SidebarLink>
             </li>
             <li>
-              <SidebarLink href="/permissions" :active="isActive('/permissions')" :class="{'bg-blue-500':isActive('/permissions'),'text-white':true}">
+              <SidebarLink href="/permissions" :active="isActive('/permissions')" :class="{'bg-blue-500':isActive('/permissions'),'text-white':true}"v-if="hasRole('admin')">
                 <template #icon>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 0 1-3.6 9.75m6.633-4.596a18.666 18.666 0 0 1-2.485 5.33" />
@@ -143,7 +143,10 @@
     import SidebarLink from "@/Components/SidebarLink.vue";
     import Dropdown from '@/Components/Dropdown.vue';
     import DropdownLink from '@/Components/DropdownLink.vue';
+    import { usePermission } from "@/composables/permissions";
+    
 
+    const { hasRole } = usePermission();
     const showingNavigationDropdown = ref(false);
     const page = usePage();
     const authUser = page.props.auth.user;
